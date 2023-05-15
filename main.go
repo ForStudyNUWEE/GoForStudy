32de12c1c4427dc83af80b9d70b70e9dcb89e325
mBoichura <mBoichura@gmail.com>
2023-05-15 19:00:58 +0300

    Filled Contacts page
    Filled Contacts page
    Added fictive function forExample

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -22,11 +21,31 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func contacts(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "09888234685")
+	tmpl, _ := template.ParseFiles("templates/contacts.html")
+
+	developer := Developer{"Бойчура М.В.", "НУВГП", 2023, "https://wiki.nuwm.edu.ua/index.php/Бойчура_Михайло_Володимирович"}
+
+	tmpl.Execute(w, developer)
 }
 
 func main() {
+	//forExample()
 	http.HandleFunc("/", index)
 	http.HandleFunc("/contacts/", contacts)
 	http.ListenAndServe(":8080", nil)
+}
+
+func forExample() {
+	//var x int
+	//var x int = 5
+	//var x = 5
+	//x := 5
+	//fmt.Print(x)
+
+	//var y Developer
+	//var y Developer = Developer{"", "", 5, ""}
+	//var y = Developer{"fsd", "ds", 5, "ds"}
+	//y := Developer{"fsd", "ds", 5, "ds"}
+	//fmt.Print(y)
+	
 }
