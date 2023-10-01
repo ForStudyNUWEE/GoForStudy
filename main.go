ee882f063211218d82ac3af207b941cb12ff60e1
mBoichura <mBoichura@gmail.com>
2023-10-01 20:53:11 +0300

    Few files

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -13,11 +14,11 @@ type Developer struct {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("templates/index.html")
+	tmpl, _ := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 
 	developer := Developer{"Бойчура М.В.", "НУВГП", 2023, "https://wiki.nuwm.edu.ua/index.php/Бойчура_Михайло_Володимирович"}
 
-	tmpl.Execute(w, developer)
+	tmpl.ExecuteTemplate(w, "index", developer)
 }
 
 func contacts(w http.ResponseWriter, r *http.Request) {
@@ -28,10 +29,22 @@ func contacts(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, developer)
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+
+				fmt.Print(r.PostForm, r.FormValue("username"), r.FormValue("password"))
+
+		tmpl, _ := template.ParseFiles("templates/login.html", "templates/header.html", "templates/footer.html")
+
+		developer := Developer{"Бойчура М.В.", "НУВГП", 2023, "https://wiki.nuwm.edu.ua/index.php/Бойчура_Михайло_Володимирович"}
+
+		tmpl.ExecuteTemplate(w, "login", developer)
+}
+
 func main() {
 	//forExample()
 	http.HandleFunc("/", index)
 	http.HandleFunc("/contacts/", contacts)
+	http.HandleFunc("/login/", login)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -47,5 +60,5 @@ func forExample() {
 	//var y = Developer{"fsd", "ds", 5, "ds"}
 	//y := Developer{"fsd", "ds", 5, "ds"}
 	//fmt.Print(y)
-	
+
 }
